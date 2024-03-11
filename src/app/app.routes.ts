1ad78d016978fd4398a56284f9cb946e582b4a6a
unknown <andrago2025@hotmail.com>
2024-03-10 18:03:49 -0600

    Added main page structure and styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './pages/home/home.page';
+import { ContactPage } from './pages/contact/contact.page';
+import { AboutPage } from './pages/about/about.page';
+import { ErrorPage } from './pages/error/error.component';
+import { LoginPage } from './pages/login/login.page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "home",component: HomePage},
+    {path: "contact",component:ContactPage},
+    {path: "about",component:AboutPage},
+    {path: "error",component:ErrorPage},
+    {path: "login",component:LoginPage},
+    {path: "",redirectTo: "home",pathMatch: "full"},
+    {path: "**",redirectTo: "error",pathMatch: "full"}
+];
