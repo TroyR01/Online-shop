eb71aba97a8b6328f25648ca65b2d1a567fe016c
Romina Pérez <A325835@alumnos.uaslp.mx>
2024-03-28 20:55:41 -0600

    Contact Us Style

--- src/app/pages/contact/contact.page.ts
+++ src/app/pages/contact/contact.page.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-
+import { FontAwesomeModule,FaIconLibrary  } from '@fortawesome/angular-fontawesome';
+import { faFacebook, faTwitter, faInstagram, faWhatsapp, faGoogle  } from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactPage {
+  faFacebook = faFacebook;
+  faTwitter = faTwitter; 
+  faInstagram=faInstagram; 
+  faWhatsapp=faWhatsapp; 
+  faGoogle=faGoogle;
 
 }
