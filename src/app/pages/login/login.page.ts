ae36dcd3c54153d7993179a7f7be17cc76cd93a1
Romina Pérez <A325835@alumnos.uaslp.mx>
2024-03-28 22:56:42 -0600

    Login Style

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgClass, NgIf } from '@angular/common';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [NgIf,NgClass],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginPage {
 
+  isSignup: boolean = true;
+
+  toggleForm() {
+    this.isSignup = !this.isSignup;
+  }
 }
